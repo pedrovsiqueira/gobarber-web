6ef041f8a5c7c0d05f1faafe9b38de44d6624943
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-11 16:28:47 -0300

    Add signup page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+// import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
